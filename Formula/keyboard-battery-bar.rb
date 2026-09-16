008fda6d40ecc8b2b378f84aa948b43c68ae0930
Or Basker <87074045+Orbasker@users.noreply.github.com>
2026-09-16 17:05:34 +0300

    Point formula at v1.1

--- Formula/keyboard-battery-bar.rb
+++ Formula/keyboard-battery-bar.rb
@@ -1,8 +1,8 @@
 class KeyboardBatteryBar < Formula
   desc "Menu bar battery level for Bluetooth keyboards, plus a CLI"
   homepage "https://github.com/Orbasker/keyboard-battery-bar"
-  url "https://github.com/Orbasker/keyboard-battery-bar/archive/refs/tags/v1.0.tar.gz"
-  sha256 "ca81315b2f1c78931b1d59deba604c1c267a999d7283ff2f9f75770cfe8533f0"
+  url "https://github.com/Orbasker/keyboard-battery-bar/archive/refs/tags/v1.1.tar.gz"
+  sha256 "7249630ca136fd9b73afa37e9ad439b66294ebdb5775c7ae9a20c6de6cee9056"
   license "MIT"
 
   depends_on xcode: :build
